fc72a6c24ecde32f9198d03957647a30f988a291
AscMar <ascona.martina@gmail.com>
2025-06-25 12:08:51 -0300

    login con api, otras modificaciones

---
+++ js/login.js
@@ -0,0 +1,41 @@
+// Verificamos si ya hay sesión activa
+if (sessionStorage.getItem('usuario')) {
+  alert('Ya habías iniciado sesión. Redirigiendo al formulario...');
+  window.location.href = 'altaSalon.html';
+}
+
+// Escuchamos el envío del formulario
+document.getElementById('loginForm').addEventListener('submit', async function(event) {
+  event.preventDefault();
+
+  const usuario = document.getElementById('usuario').value;
+  const contrasena = document.getElementById('contrasena').value;
+
+  try {
+    const response = await fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            username: usuario,
+            password: contrasena
+  })
+});
+
+    const data = await response.json();
+    console.log(data);
+
+
+    if (response.ok) {
+      sessionStorage.setItem('usuario', data.username);
+      sessionStorage.setItem('token', data.token);
+      alert('Inicio de sesión exitoso');
+      window.location.href = 'altaSalon.html';
+    } else {
+      alert('Error: ' + (data.message || 'Usuario o contraseña incorrectos'));
+    }
+  } catch (error) {
+    console.error('Error en la conexión:', error);
+    alert('No se pudo establecer conexión con el servidor');
+  }
+  
+});
